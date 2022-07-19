7fedf0bb9fbcd0602eb98646c3a2ca7074c1d747
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-07-18 22:09:35 -0700

    Library: Header, Add Button, Overlay

--- 8_Library/script.js
+++ 8_Library/script.js
@@ -1,2 +1,43 @@
 'use strict';
-alert('Working JS');
+
+// Selecting Elements
+const addButton = document.querySelector('.add-book-button');
+const overlay = document.querySelector('.overlay');
+
+// modal
+addButton.addEventListener('click', () => {
+  overlay.classList.remove('hidden');
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+});
+
+let myLibrary = [];
+
+const Book = function (title, author, numOfPages, read) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.read = read;
+};
+
+const addBookToLibrary = function (obj) {
+  myLibrary.push(obj);
+};
+
+const displayAllBooks = function (arrayOfBooks) {};
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.read}`;
+};
+
+const theHobbit = new Book(
+  'The Hobbit',
+  'J.R.R. Tolkien',
+  295,
+  'has not read yet'
+);
+console.log(theHobbit.info());
+addBookToLibrary(theHobbit);
+console.log(myLibrary);
